cc34bc7e50abb49725e1faadac6f7d6299a16a82
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-07-11 23:19:20 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+document.querySelector(".name").innerHTML="NAMAN SHARMA";
+document.querySelector(".roll").innerHTML="201500428";
+document.querySelector(".college").innerHTML="GLA University, Mathura";
+const d= new Date();
+
+var date = new Date();
+var n = date.toDateString();
+ document.querySelector(".date").innerHTML=n;
